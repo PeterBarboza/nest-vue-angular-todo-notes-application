e4e293f9e1866cfbcd154edc89dcc7b9984e9eca
PeterBarboza <pedrom.barboza01@gmail.com>
2024-02-13 13:28:07 -0300

    style/feat: Components folder structure changed; Global top-bar added

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { SignInComponent } from './sign-in/sign-in.component'
-import { SignUpComponent } from './sign-up/sign-up.component'
-import { AppComponent } from './app.component';
-import { WebsiteComponent } from './website/website.component';
+import { SignInComponent } from './components/modules/sign-in/sign-in.component'
+import { SignUpComponent } from './components/modules/sign-up/sign-up.component'
+import { WebsiteComponent } from './components/modules/website/website.component';
 
 export const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
